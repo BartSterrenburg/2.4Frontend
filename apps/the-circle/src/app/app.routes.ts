c8295058125705d5986950f2f680e3749d87179f
AlbanorZabeli <151035773+AlbanorZabeli@users.noreply.github.com>
2025-06-16 14:10:24 +0200

    startscreen

--- apps/the-circle/src/app/app.routes.ts
+++ apps/the-circle/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Route } from '@angular/router';
+import { Startscreen } from '../lib/startscreen/startscreen';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {path: '', component: Startscreen},
+];
