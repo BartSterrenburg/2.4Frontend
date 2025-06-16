539a7db5af4cd3b5ce14c517ea3c323d6ef4a1b5
AlbanorZabeli <151035773+AlbanorZabeli@users.noreply.github.com>
2025-06-16 13:09:13 +0200

    components

---
+++ apps/the-circle/src/lib/startscreen/startscreen.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-startscreen',
+  imports: [CommonModule],
+  templateUrl: './startscreen.html',
+  styleUrl: './startscreen.css',
+})
+export class Startscreen {}
