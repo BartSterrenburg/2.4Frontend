612efb1b0a3db2719a3ee19ea0ce6a2304faebf1
barts <barts@BARTOS>
2025-06-17 09:42:20 +0200

    added create signature function

--- shared/services/src/lib/services.ts
+++ shared/services/src/lib/services.ts
@@ -1,3 +1,10 @@
-export function services(): string {
-  return 'services';
+import { createSign } from 'crypto';
+
+export class Services {
+  createSignature(data: string, privKey: string): string {
+    const signer = createSign('SHA256');
+    signer.update(data);
+    signer.end();
+    return signer.sign(privKey, 'base64');
+  }
 }
