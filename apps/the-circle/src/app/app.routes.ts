5893eea4e53b9d8d84a535ab55e3eb14fd1245a7
AlbanorZabeli <151035773+AlbanorZabeli@users.noreply.github.com>
2025-06-17 11:31:31 +0200

    Update app.routes.ts

--- apps/the-circle/src/app/app.routes.ts
+++ apps/the-circle/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Route } from '@angular/router';
 import { Startscreen } from '../lib/startscreen/startscreen';
+import { LoginComponent } from '../lib/login/login';
 
 export const appRoutes: Route[] = [
     {path: '', component: Startscreen},
+    {path: 'login', component: LoginComponent},
 ];
